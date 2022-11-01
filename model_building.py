f709ca3d761d78c4a82a9112290c4f840f006fc3
Shubham Undirwade <89636942+shubhamundirwade@users.noreply.github.com>
2022-11-01 22:58:17 +0530

    Update model_building.py

--- model_building.py
+++ model_building.py
@@ -2,11 +2,14 @@
 import numpy as np
 import pandas as pd
 import scipy
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+
 from sklearn.preprocessing import  StandardScaler
 from scipy.cluster.hierarchy import  dendrogram, linkage
+from sklearn.cluster import  KMeans
 
 ## Importing dataset
 df_segmentation = pd.read_csv("segmentation data.csv", index_col=0)
@@ -66,4 +69,51 @@ dendrogram(hier_clust,
             show_leaf_counts = True,
             no_labels = True
             )
+plt.show()
+
+## K-Means Clustering
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state=42)
+    kmeans.fit(segmentation_std)
+    wcss.append(kmeans.inertia_)
+    
+#Ploting for number of clusters 
+plt.figure(figsize=(10,8))
+plt.plot(range(1,11), wcss, marker = 'o', linestyle = '--')
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.title("K-Means Clustering")
+plt.show()
+
+kmeans = KMeans(n_clusters=4, init= 'k-means++', random_state=42)
+kmeans.fit(segmentation_std)
+
+## Results
+ df_segm_kmeans = df_segmentation.copy()
+ df_segm_kmeans["Segment K-means"] = kmeans.labels_
+
+df_segm_analysis = df_segm_kmeans.groupby(["Segment K-means"]).mean()
+df_segm_analysis
+
+df_segm_analysis["N Obs"] = df_segm_kmeans[["Segment K-means", "Sex"]].groupby(["Segment K-means"]).count()
+df_segm_analysis["Prop Obs"] = df_segm_analysis["N Obs"] / df_segm_analysis["N Obs"].sum()
+
+#renaming the column names
+df_segm_analysis.rename({0:"well-off",
+                         1:"fewer-opportunities",
+                         2: 'standard',
+                         3: "career focused"})
+
+#lets plot raw data 
+df_segm_kmeans["Labels"] = df_segm_kmeans["Segment K-means"].map({0:"well-off",
+                                                                  1:"fewer-opportunities",
+                                                                  2: 'standard',
+                                                                  3: "career focused"})
+
+x_axis = df_segm_kmeans["Age"]
+y_axis = df_segm_kmeans["Income"]
+plt.figure(figsize=(10,8) )
+sns.scatterplot(x_axis, y_axis, hue=df_segm_kmeans["Labels"], palette=['g','r','c','m'])
+plt.title("Segmentation K-means")
 plt.show()
